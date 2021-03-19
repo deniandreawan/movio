3a35e23fe00c52cd085ce31a682437154f5a4c07
Deni Andreawan <deniandreawan0@gmail.com>
2021-03-20 04:28:26 +0700

    ⚡ Add Movie Poster

--- src/pages/movie/ViewMovie.js
+++ src/pages/movie/ViewMovie.js
@@ -13,19 +13,24 @@ import useDocumentTitle from "hooks/useDocumentTitle";
 import useDidMount from "hooks/useDidMount";
 
 const ViewMovie = (props) => {
-  const { favorites, movie, casts, keywords, reviews, isLoading } = useSelector((state) => ({
-    favorites: state.favorites,
-    movie: state.movies.current.movie,
-    casts: state.movies.current.casts,
-    keywords: state.movies.current.keywords,
-    reviews: state.movies.current.reviews,
-    isLoading: state.loading.isLoading,
-  }));
+  const { favorites, movie, casts, keywords, reviews, isLoading } = useSelector(
+    (state) => ({
+      favorites: state.favorites,
+      movie: state.movies.current.movie,
+      casts: state.movies.current.casts,
+      keywords: state.movies.current.keywords,
+      reviews: state.movies.current.reviews,
+      isLoading: state.loading.isLoading,
+    })
+  );
   const dispatch = useDispatch();
   const didMount = useDidMount();
+  const posters = movie.images ? movie.images.posters : [];
 
   useDocumentTitle(
-    movie.id ? `${movie.original_name || movie.original_title} | Movio` : "View Movie | Movio"
+    movie.id
+      ? `${movie.original_name || movie.original_title} | Movio`
+      : "View Movie | Movio"
   );
   useEffect(() => {
     const movieId = props.match.params.id;
@@ -46,18 +51,41 @@ const ViewMovie = (props) => {
     }
   };
 
+  const onClickViewImage = () => {
+    props.history.push(`/view/movie/${props.match.params.id}/images`);
+  };
+
   return !isLoading ? (
     <>
       <MovieOverview favorites={favorites} movie={movie} />
       <MovieCast casts={casts} keywords={keywords} movie={movie} />
+      {movie.images && (
+        <div className="container__wrapper">
+          <MoviePoster
+            id={movie.id}
+            posters={posters.length > 8 ? posters.slice(0, 8) : posters}
+          />
+          <button
+            className="button--primary button--block m-auto"
+            onClick={onClickViewImage}
+          >
+            View All Posters
+          </button>
+        </div>
+      )}
       {movie.similar && (
         <>
           {movie.similar.results.length !== 0 && (
-            <SimilarMovies favorites={favorites} movies={movie.similar.results} />
+            <SimilarMovies
+              favorites={favorites}
+              movies={movie.similar.results}
+            />
           )}
         </>
       )}
-      {reviews.results && !!reviews.total_pages && <Reviews reviews={reviews} />}
+      {reviews.results && !!reviews.total_pages && (
+        <Reviews reviews={reviews} />
+      )}
     </>
   ) : (
     <MovieOverview favorites={[]} movie={{}} />
